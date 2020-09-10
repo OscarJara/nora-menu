9920a1d4580b968dd49210a5f1d83c72a2270af1
Oscar Jara <ojara@cognitiva.la>
2020-09-10 13:10:38 -0300

    ADD: Menu & Option models

--- applications/menu/models.py
+++ applications/menu/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+
+class Option(models.Model):
+    
+    description = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'option'
+        verbose_name = 'option'
+        verbose_name_plural = 'options'
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+
+    id = models.UUIDField( 
+        primary_key = True, 
+        default = uuid.uuid4, 
+        editable = False
+    )     
+    date = models.DateField()
+    options = models.ManyToManyField(Option)
+
+    class Meta:
+        db_table = 'menu'
+        verbose_name = 'menu'
+        verbose_name_plural = 'menus'
+        
+    def __str__(self):
+        return self.id + ' ' + self.date + ' ' + self.options
