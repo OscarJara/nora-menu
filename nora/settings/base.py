5e8c799b87aa889c707ef2cef27615526b94f2a9
Oscar Jara <ojara@cognitiva.la>
2020-09-10 13:51:18 -0300

    ADD: templates directory

--- nora/settings/base.py
+++ nora/settings/base.py
@@ -49,7 +49,9 @@ ROOT_URLCONF = 'nora.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.child('templates')    
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
