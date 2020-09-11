2970eed6f4f8dfbee0dba19dbf22884bcb570f37
Oscar Jara <ojara@cognitiva.la>
2020-09-11 14:20:37 -0300

    DOCS: code explanation is addes, what does each test do

--- applications/users/tests/test_views.py
+++ applications/users/tests/test_views.py
@@ -15,6 +15,9 @@ from applications.users.models import User
 class UserViewTest(TestCase):
     
     def setUp(self):
+        '''
+            A base user is created, with id to later use it in the edit an delete
+        '''
         new_user = User()
         new_user.full_name = 'testUser1'
         new_user.mail = 'test@test.cl'
@@ -24,6 +27,10 @@ class UserViewTest(TestCase):
         self.user_id = new_user.id
         
     def test_create_user(self):
+        '''
+            A new user is created through the add-user view,
+            a creation without error is expected, with return code 302
+        '''
         payload_test = {
             "full_name": "testUser2",
             "mail": "test@test.cl",
@@ -34,6 +41,11 @@ class UserViewTest(TestCase):
         self.assertEqual(response.status_code,302,'the returned code is not what we expected')
         
     def test_create_user_emptyFields(self):
+        '''
+            a new user is created through the add-user view,
+            an error is expected in the creation, with return code 200,
+            because the fields are beign sent empty
+        '''
         payload_test = {
             "full_name": "",
             "mail": "",
@@ -44,6 +56,11 @@ class UserViewTest(TestCase):
         self.assertEqual(response.status_code,200,'the returned code is not what we expected')
         
     def test_create_user_incorrectData(self):
+        '''
+            a new user is created through the add-user view,
+            an error is expected in the creation, with return code 200,
+            because the fields are incorrect and incomplete
+        '''
         payload_test = {
             "full_name": 22211,
             "mail": "",
@@ -53,11 +70,17 @@ class UserViewTest(TestCase):
         self.assertEqual(response.status_code,200,'the returned code is not what we expected')
         
     def test_list_users(self):
+        '''
+            list user through the url home,
+            no error expected, with return code 200
+        '''
         response = self.client.get(reverse('user_app:home'))
         self.assertEqual(response.status_code,200,'the returned code is not what we expected')
         
     def test_update_user(self):
-        
+        '''
+            Tue user that was created when starting the setUp is modified, no error is expected, with return code 302
+        '''
         payload_update_test = {
             "full_name": "UserTest1",
             "mail": "test@test.cl",
@@ -70,7 +93,9 @@ class UserViewTest(TestCase):
         self.assertEqual(response.status_code,302,'the returned code is not what we expected')
     
     def test_delete_user(self):
-        
+        '''
+            User created when starting the setUp is deleted, no error is expected, with return code 302
+        '''
         url_delete = '/delete-user/%s' %(str(self.user_id))
         response = self.client.post(url_delete)
         self.assertEqual(response.status_code,302,'the returned code is not what we expected')
