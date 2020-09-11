e71c86f763c7ddd5c39acc8515c7668dd5f288f8
Oscar Jara <ojara@cognitiva.la>
2020-09-11 12:24:45 -0300

    REF: change class name, ADD: validation of inputs

--- applications/users/forms.py
+++ applications/users/forms.py
@@ -4,7 +4,7 @@ from .models import User, UserMenu
 
 import re
 
-class CreateUser(forms.ModelForm):
+class BaseUserForm(forms.ModelForm):
     
     class Meta:
         model = User
@@ -44,6 +44,19 @@ class CreateUser(forms.ModelForm):
         mail = self.cleaned_data['mail']
         validate_mail = re.match(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",mail)
         if not validate_mail:
-            raise forms.ValidationError('Ingrese un email valido.')
+            raise forms.ValidationError('El email debe ser valido')
         
         return mail
+    def clean_full_name(self):
+        
+        full_name = self.cleaned_data['full_name']
+        if len(full_name) <5:
+            raise forms.ValidationError('Ingrese un nombre más largo')
+        return full_name
+    
+    def clean_password(self):
+        
+        password = self.cleaned_data['password']
+        if len(password)<8:
+            raise forms.ValidationError('La contraseña debe tener un largo minimo de 8')        
+        return password
