bdc8c233905b3276fd072fa7237d080bb74e2d5d
Oscar Jara <ojara@cognitiva.la>
2020-09-10 21:18:19 -0300

    ADD: createUserView

--- applications/users/views.py
+++ applications/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import User
+
+from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView,
+    CreateView
+)
+
+from .models import User
+from .forms import CreateUser
+
+
+class CreateUserView(CreateView):
+    
+    template_name = 'users/add.html'
+    model = User
+    form_class = CreateUser
+    
+    success_url = reverse_lazy('user_app:home')
+    
+
+        
