f3afccbd78906301778f0db9302a54ea69eb0d6d
Oscar Jara <ojara@cognitiva.la>
2020-09-10 21:51:32 -0300

    ADD: base url to views

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 app_name = 'user_app'
 
 urlpatterns = [
-    
+    path('add-user/', views.CreateUserView.as_view(),name='add'),
+    path('home-user/', views.ListUserView.as_view(),name='home'),
+    path('delete-user/<pk>', views.UserDeleteView.as_view(),name='delete'),
+    path('update-user/<pk>', views.UpdateUserView.as_view(),name='update')
 ]
