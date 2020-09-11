d5b821dc34169ced83d8f30ccb34f79dff3dde30
Oscar Jara <ojara@cognitiva.la>
2020-09-10 21:51:53 -0300

    ADD: generics views, list,user,delete

--- applications/users/views.py
+++ applications/users/views.py
@@ -5,7 +5,10 @@ from .models import User
 from django.urls import reverse_lazy
 from django.views.generic import (
     TemplateView,
-    CreateView
+    CreateView,
+    ListView,
+    DeleteView,
+    UpdateView,
 )
 
 from .models import User
@@ -20,5 +23,21 @@ class CreateUserView(CreateView):
     
     success_url = reverse_lazy('user_app:home')
     
+class ListUserView(ListView):
 
+    template_name = 'users/users.html'
+    model = User
+    context_object_name = 'users'
         
+class UserDeleteView(DeleteView):
+    
+    template_name = "users/delete.html"
+    model = User
+    success_url = reverse_lazy('user_app:home')
+
+
+class UpdateUserView(UpdateView):
+    template_name = "users/update.html"
+    model = User
+    form_class = CreateUser
+    success_url = reverse_lazy('user_app:home')
