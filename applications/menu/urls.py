8f0fce5d14d9603c18b0da9a1da06c0d50db7e5d
Oscar Jara <ojara@cognitiva.la>
2020-09-11 23:26:33 -0300

    ADD: urls with views and views with rest_framework

--- applications/menu/urls.py
+++ applications/menu/urls.py
@@ -6,5 +6,16 @@ from . import views
 app_name = 'menu_app'
 
 urlpatterns = [
-    
+    path('add-options/',views.CreateOptionsView.as_view(),name='add_options'),
+    path('options/',views.ListOptionsView.as_view(),name='options'),
+    # path('menus/',views.ListMenusView.as_view(),name='menus'),
+    path('add-menu/',views.CreateMenuView.as_view(),name='add_menu'),
+    path('API/menus/',views.ListMenuAPIView.as_view(),name='menus'),
 ]
+
+# urlpatterns = [
+#     path('add-user/', views.CreateUserView.as_view(),name='add'),
+#     path('home-user/', views.ListUserView.as_view(),name='home'),
+#     path('delete-user/<pk>', views.UserDeleteView.as_view(),name='delete'),
+#     path('update-user/<pk>', views.UpdateUserView.as_view(),name='update')
+# ]
