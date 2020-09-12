c0dd18df7ba14f264506c7f64d82ad14433900ba
Oscar Jara <ojara@cognitiva.la>
2020-09-11 23:25:04 -0300

    ADD: generics views and views using rest_framework

--- applications/menu/views.py
+++ applications/menu/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView, 
+    CreateView,
+    ListView,
+    DeleteView,
+    UpdateView
+)
+
+from rest_framework.generics import ListAPIView
+from .serializers import MenuSerializer
+
+from .models import (
+    Menu,
+    Option
+)
+from .forms import (
+    BaseOptionForm,
+    BaseMenuForm
+)
+
+class CreateOptionsView(CreateView):
+    
+    template_name = 'menu/options/add.html'
+    model = Option
+    form_class = BaseOptionForm
+    
+    success_url = reverse_lazy('menu_app:options')
+    
+class ListOptionsView(ListView):
+    
+    template_name = 'menu/options/options.html'
+    model = Option
+    context_object_name = 'options'
+    
+class ListMenuAPIView(ListAPIView):
+
+    serializer_class = MenuSerializer
+    
+    def get_queryset(self):
+        
+        return Menu.objects.all()
+    
+    
+class CreateMenuView(CreateView):
+    
+    template_name = 'menu/add.html'
+    model = Menu
+    form_class = BaseMenuForm
+    
+    success_url = reverse_lazy('menu_app:menus')
