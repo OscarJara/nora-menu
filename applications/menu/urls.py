1257951b92120ff0c011edae9dea2b3538a772cb
Oscar Jara <ojara@cognitiva.la>
2020-09-12 18:18:40 -0300

    ADD: url to api and forms model

--- applications/menu/urls.py
+++ applications/menu/urls.py
@@ -8,14 +8,11 @@ app_name = 'menu_app'
 urlpatterns = [
     path('add-options/',views.CreateOptionsView.as_view(),name='add_options'),
     path('options/',views.ListOptionsView.as_view(),name='options'),
-    # path('menus/',views.ListMenusView.as_view(),name='menus'),
     path('add-menu/',views.CreateMenuView.as_view(),name='add_menu'),
-    path('API/menus/',views.ListMenuAPIView.as_view(),name='menus'),
+    path('API/menus/',views.ListMenuAPIView.as_view(),name='api_menu'),
+    path('API/specific/',views.SpecificMenuAPIView.as_view(),name='api_specific'),
+    path('menus/',views.MainMenuView.as_view(),name='menus'),
+    path('delete-menu/<pk>',views.DeleteMenuView.as_view(),name='delete_menu'),
+    path('update-menu/<pk>',views.UpdateMenuView.as_view(),name='update_menu')
+    
 ]
-
-# urlpatterns = [
-#     path('add-user/', views.CreateUserView.as_view(),name='add'),
-#     path('home-user/', views.ListUserView.as_view(),name='home'),
-#     path('delete-user/<pk>', views.UserDeleteView.as_view(),name='delete'),
-#     path('update-user/<pk>', views.UpdateUserView.as_view(),name='update')
-# ]
