07a9028ab08c99e0f809808818f04cc50a0b6170
Oscar Jara <ojara@cognitiva.la>
2020-09-13 00:11:00 -0300

    ADD: new url, home

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('update-user/<pk>', views.UpdateUserView.as_view(),name='update'),
     path('API/add-select/',views.CreateUserMenuAPIView.as_view(),name='user_name'),
     path('API/user-menu/',views.ListUserMenuAPIView.as_view(),name='user_menu'),
-    path('menus-selected',views.SelectMenuListView.as_view(),name='menus_selected'),
+    # path('menus-selected',views.SelectMenuListView.as_view(),name='menus_selected'),
+    path('home/',views.HomeView.as_view(),name='menus_selected')
 ]
