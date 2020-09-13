f8c64a6a74bbddad14c91b8ace62df214a0fa146
Oscar Jara <ojara@cognitiva.la>
2020-09-12 23:55:38 -0300

    ADD: new url to menus selected

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('delete-user/<pk>', views.UserDeleteView.as_view(),name='delete'),
     path('update-user/<pk>', views.UpdateUserView.as_view(),name='update'),
     path('API/add-select/',views.CreateUserMenuAPIView.as_view(),name='user_name'),
-    path('API/user-menu/',views.ListUserMenuAPIView.as_view(),name='user_menu')
+    path('API/user-menu/',views.ListUserMenuAPIView.as_view(),name='user_menu'),
+    path('menus-selected',views.SelectMenuListView.as_view(),name='menus_selected'),
 ]
