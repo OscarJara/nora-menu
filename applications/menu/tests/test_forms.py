a7e99e41fb06db1499cf968ad1dbb64abaca97a8
Oscar Jara <ojara@cognitiva.la>
2020-09-13 18:15:41 -0300

    ADD: menu testing

--- applications/menu/tests/test_forms.py
+++ applications/menu/tests/test_forms.py
@@ -4,6 +4,7 @@ from applications.menu.forms import (
     BaseOptionForm,
     BaseMenuForm
 )
+import datetime
 
 class CreateOptionTest(TestCase):
    
@@ -37,3 +38,69 @@ class CreateOptionTest(TestCase):
             ['This field is required.'],
             'unexpected error response'
         ) 
+
+class CreateMenuTest(TestCase):
+    
+    def test_MenuForm(self):
+        '''
+            In this test a incorrect form is generated,
+            the error response must be empty
+            
+            Incorrect option.
+        '''
+        form = BaseMenuForm(
+            data = {
+                'date':datetime.date.today(),
+                'options':[4]
+            }
+        )
+        self.assertEqual(
+            form.errors['options'],
+            ['Select a valid choice. 4 is not one of the available choices.'],
+            'unexpected error response'
+        )
+        
+    def test_MenuForm(self):
+        '''
+            In this test a incorrect form is generated,
+            the error response must be empty
+            
+            Empty option.
+        '''
+        form = BaseMenuForm(
+            data = {
+                'date':datetime.date.today(),
+                'options':''
+            }
+        )
+        self.assertEqual(
+            form.errors['options'],
+            ['This field is required.'],
+            'unexpected error response'
+        )
+        
+    def test_MenuForm(self):
+        '''
+            In this test a incorrect form is generated,
+            the error response must be empty.
+            
+                * date with previous date
+                * Empty option.
+        '''
+        previous_date = datetime.date.today() - datetime.timedelta(days=1)
+        form = BaseMenuForm(
+            data = {
+                'date':previous_date,
+                'options':''
+            }
+        )
+        self.assertEqual(
+            form.errors['options'],
+            ['This field is required.'],
+            'unexpected error response'
+        )
+        self.assertEqual(
+            form.errors['date'],
+            ['La fecha ingresada para crear el menu no puede ser para una anterior a la de hoy'],
+            'unexpected error response'
+        )
